c46dfc27d240656492f38f7e034a7d3ffac82047
Averone30 <zhafirfza@gmail.com>
2026-02-08 03:04:28 +0700

    [update & fix] Clean up NetworkModule by removing unused SSL bypass code and improving comments - Delete unnecessary code (beta) - Now it's more secure (I think).

--- app/src/main/java/com/kpri/binasejahtera/di/NetworkModule.kt
+++ app/src/main/java/com/kpri/binasejahtera/di/NetworkModule.kt
@@ -13,61 +13,28 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
-
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    // pake URL API yg asli biar bisa diakses (bagian akhir kasih "/")
+    // pastikan URL diakhiri dengan "/" (soalnya tadi bingung kenapa kok g bisa)
     private const val BASE_URL = "https://be-presensi-beta.trisuladana.com/api/"
 
     @Provides
     @Singleton
     fun provideLoggingInterceptor(): HttpLoggingInterceptor {
         return HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY // untuk debugging, nge-print request/response di Logcat
+            level = HttpLoggingInterceptor.Level.BODY
         }
     }
 
-//    @Provides
-//    @Singleton
-//    fun provideOkHttpClient(
-//        loggingInterceptor: HttpLoggingInterceptor,
-//        authInterceptor: AuthInterceptor
-//    ): OkHttpClient {
-//        return OkHttpClient.Builder()
-//            .addInterceptor(authInterceptor)
-//            .addInterceptor(loggingInterceptor)
-//            .connectTimeout(30, TimeUnit.SECONDS)
-//            .readTimeout(30, TimeUnit.SECONDS)
-//            .build()
-//    }
-
-    // beta test
     @Provides
     @Singleton
     fun provideOkHttpClient(
         loggingInterceptor: HttpLoggingInterceptor,
         authInterceptor: AuthInterceptor
     ): OkHttpClient {
-        // --- BUAT TRUST MANAGER YANG PERCAYA SEMUA CERT ---
-        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-        })
-
-        val sslContext = SSLContext.getInstance("SSL")
-        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
-        val sslSocketFactory = sslContext.socketFactory
-
         return OkHttpClient.Builder()
-            .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager) // Bypass SSL
-            .hostnameVerifier { _, _ -> true } // Bypass Hostname
             .addInterceptor(authInterceptor)
             .addInterceptor(loggingInterceptor)
             .connectTimeout(30, TimeUnit.SECONDS)
